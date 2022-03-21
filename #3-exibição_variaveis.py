ff476ef40873c4174257e7bc72982aacd95f3f60
pipp3r <pipperfernandoalmeida@gmail.com>
2022-03-21 12:12:19 -0300

    treinando exibiçao de variaveis

---
+++ #3-exibição_variaveis.py
@@ -0,0 +1,13 @@
+# treino de exibição de variáveis
+
+
+nome = input("Digite o nome do funcionário:")
+codigo = int(input("Digite o código do funcionário:"))
+salario = float(input("digíte o salário do Funcionário:"))
+ativo = True
+print("\n")
+
+print("Código: %d \n" % codigo,)
+print("Nome: %s \n" % nome)
+print("Salário: %.2f \n" % salario)
+print("está ativo: %r \t" % ativo)
