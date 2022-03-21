9dc5cfb974e20a1d12d862d98c6ee0334323ecf9
pipp3r <pipperfernandoalmeida@gmail.com>
2022-03-21 12:13:07 -0300

    treinando operadores logicos

---
+++ #4-operadores_lógicos.py
@@ -0,0 +1,12 @@
+# treinando a lógica dos operadores
+
+A = 25
+B = 25
+C = 30
+
+print("A==B AND B > C:", A == B and B > C)
+print("\n")
+print("A<B OR B>C:", A < B or B > C)
+print("\n")
+print("NOT A==B", not A==B)
+print("\n")
