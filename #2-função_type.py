1618858937d04c55b12d637b640b434607c7ca0e
pipp3r <pipperfernandoalmeida@gmail.com>
2022-03-21 12:11:04 -0300

    identificando tipos de variaveis com a funçao type

---
+++ #2-função_type.py
@@ -0,0 +1,10 @@
+# identificando tipos de variaveis com a função type
+
+
+int (inteiros), float(números reais), bool(booleanos, verdadeiro ou falso) e str(srings/char, para caracteres).
+codigo =(10)
+salario = 1500
+nome = 'José'
+tipo = type (codigo)
+
+print("o nome é" + str (nome), "o salário é de:", salario, "o código é:", codigo, "o tipo do código é um:", tipo)
